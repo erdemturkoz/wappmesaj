142e76682dc94f5548303e5aadb31d219e797aa5
erdemturkoz <turkozerdem@gmail.com>
2026-07-29 20:15:54 +0300

    feat: add realtime disconnect handler

---
+++ backend/realtime.ts
@@ -0,0 +1,2 @@
+import { removeSubscriptionsByConnection } from './realtime-subscribers';
+export const realtime = async (event: any) => { let msg: any = {}; try { msg = JSON.parse(event.body || '{}'); } catch {} if (msg.type === 'system.disconnected' && msg.payload?.connection_id) await removeSubscriptionsByConnection(msg.payload.connection_id); return { statusCode: 200 }; };
